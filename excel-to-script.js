0e6ab87fd483747eef0de0606fa1dc6f5c48831f
matias doto <matydoto@gmail.com>
2022-10-08 22:21:13 -0300

    capturo errores y agrego msj de proceso finalizado correctamente

--- excel-to-script.js
+++ excel-to-script.js
@@ -4,27 +4,32 @@ import { createWriteStream } from 'fs';
 const { readFile, utils } = reader;
 
 const execute = () => {
-      const file = readFile('./resources/paradas.xlsx');
-      let sheetArray = [];
-      file.SheetNames.forEach((sheetName) => {
-            sheetArray = [...sheetArray, ...utils.sheet_to_json(file.Sheets[sheetName])];
-      });
+      try {
+            const file = readFile('./resources/paradas.xlsx');
+            let sheetArray = [];
+            file.SheetNames.forEach((sheetName) => {
+                  sheetArray = [...sheetArray, ...utils.sheet_to_json(file.Sheets[sheetName])];
+            });
 
-      const stream = createWriteStream('./resources/paradas.sql');
-      stream.write('INSERT INTO Parada (CodigoParada, Latitud, Longitud, Descripcion, IdTipoCoordenada) VALUES ');
-      sheetArray.forEach((row, index) => {
-            if (!row.valido)
-                  return;
+            const stream = createWriteStream('./resources/paradas.sql');
+            stream.write('INSERT INTO Parada (CodigoParada, Latitud, Longitud, Descripcion, IdTipoCoordenada) VALUES ');
+            sheetArray.forEach((row, index) => {
+                  if (!row.valido)
+                        return;
 
-            stream.write(`('${row.codigo}', ${row.latitud}, ${row.longitud}, '${row.descripcion}', ${row.tipo})`);
-            if (index < sheetArray.length - 1) {
-                  stream.write(',');
-                  stream.write('\r\n');
-            }
-      });
-      stream.end();
+                  stream.write(`('${row.codigo}', ${row.latitud}, ${row.longitud}, '${row.descripcion}', ${row.tipo})`);
+                  if (index < sheetArray.length - 1) {
+                        stream.write(',');
+                        stream.write('\r\n');
+                  }
+            });
+            stream.end();
 
-      return sheetArray;
+            console.log('Proceso de generación de script finalizado con éxito');
+            return sheetArray;
+      } catch (error) {
+            console.log('Error en proceso de generación de script sql', error);
+      }
 }
 
 execute();
