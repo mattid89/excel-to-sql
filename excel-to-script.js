c710f538546dc3d5b0d6aac0fba58ded137ac16d
matias doto <matydoto@gmail.com>
2024-08-29 02:16:38 -0300

    agrego generación de scripts para tabla linearamalparada. no genero idParada porque ese se genera con el identity de la tabla. separo funcionalidades en funciones separadas.

--- excel-to-script.js
+++ excel-to-script.js
@@ -10,26 +10,49 @@ const execute = () => {
             file.SheetNames.forEach((sheetName) => {
                   sheetArray = [...sheetArray, ...utils.sheet_to_json(file.Sheets[sheetName])];
             });
-
-            const stream = createWriteStream('./resources/paradas.sql');
-            stream.write('INSERT INTO Parada (CodigoParada, Latitud, Longitud, Descripcion, IdTipoCoordenada) VALUES ');
-            sheetArray.forEach((row, index) => {
-                  if (!row.valido)
-                        return;
-
-                  stream.write(`('${row.codigo}', ${row.latitud}, ${row.longitud}, '${row.descripcion}', ${row.tipo})`);
-                  if (index < sheetArray.length - 1) {
-                        stream.write(',');
-                        stream.write('\r\n');
-                  }
-            });
-            stream.end();
-
-            console.log('Proceso de generación de script finalizado con éxito');
-            return sheetArray;
+            createStops(sheetArray);
+            createLineBranchStopRelationship(sheetArray);
       } catch (error) {
             console.log('Error en proceso de generación de script sql', error);
       }
+}
+
+const createStops = (sheetArray) => {
+      const stream = createWriteStream('./resources/paradas.sql');
+      stream.write('INSERT INTO Parada (CodigoParada, Latitud, Longitud, Descripcion, IdTipoCoordenada) VALUES ');
+      sheetArray.forEach((row, index) => {
+            if (!row.valido)
+                  return;
+
+            stream.write(`('${row.codigo}', ${row.latitud}, ${row.longitud}, '${row.descripcion}', ${row.tipo})`);
+            if (index < sheetArray.length - 1) {
+                  stream.write(',');
+                  stream.write('\r\n');
+            }
+      });
+      stream.end();
+      console.log('Proceso de generación de script de paradas finalizado con éxito');
+}
+
+const createLineBranchStopRelationship = (sheetArray) => {
+      const stream = createWriteStream('./resources/linearamalparada.sql');
+
+      stream.write('INSERT INTO LineaRamalParada (IdLineaRamal, IdParada, Seccion) VALUES ');
+
+      sheetArray.forEach((row, index) => {
+            if (!row.valido)
+                  return;
+
+            stream.write(`('${row.ramal}', <IDPARADA>, 1)`);
+            if (index < sheetArray.length - 1) {
+                  stream.write(',');
+                  stream.write('\r\n');
+            }
+      });
+
+      stream.end();
+
+      console.log('Proceso de generación de script de lineaRamalParada finalizado con éxito');
 }
 
 execute();
